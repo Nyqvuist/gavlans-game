ebf99bcead2763078a5348dbe814dca7ecd47c70
Hassan Shirazi <shiraziahassan@gmail.com>
2022-10-18 00:21:52 -0400

    fixed express

--- server/index.js
+++ server/index.js
@@ -1,26 +1,16 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const io = require('socket.io')(3001, {
-    cors: {
-        origin: ['http://localhost:3000']
-    }
-})
 const mongoose = require("mongoose");
-const Player = require("./schemas/Player")
+const Player = require("./Models/Player")
 require('dotenv').config()
+
 
 mongoose.connect(process.env.MONGO, () => {
     console.log("Database is connected.")
     
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected')
-});
-
-server.listen(3000, () =>
-    console.log('server running => https://localhost:3000')
+app.listen(3000, () =>
+    console.log('Server is online.')
 );
 
