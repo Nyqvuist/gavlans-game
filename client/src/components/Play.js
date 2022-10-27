cf432da71294136b7f0ddfdb6acb293004c43628
Hassan Shirazi <shiraziahassan@gmail.com>
2022-10-27 03:12:28 -0400

    data setup

--- client/src/components/Play.js
+++ client/src/components/Play.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { useEffect } from 'react';
-import { blue } from '@mui/material/colors';
+import { Grid, Typography, Button } from '@mui/material';
+
 
 export default function Play() {
 
@@ -12,6 +13,7 @@ export default function Play() {
     const [index, setIndex] = useState(0)
     const [isBusy, setIsBusy] = useState(true)
     const [score, setScore] = useState(0)
+    const [showScore, setShowScore] = useState(false)
 
     useEffect(() => {
       setIsBusy(true)
@@ -36,19 +38,79 @@ export default function Play() {
     choices?.push(question[index].answer)
     let shuffles = choices?.sort(() => 0.5 - Math.random())
     
-    const jobDone = () => {
-      setIndex(index + 1)
-      setScore(score + 100)
+    const jobDone = (shuffle) => {
+      if(shuffle === answer){
+        setIndex(index + 1)
+        setScore(score + 100)
+        if(index === 4){
+          setShowScore(true);
+        }
+      }
+      
     };
+
     
 
-  return (
-    <div>
-      {isBusy ? (<div>hello</div>) : (<h1 style={{backgroundColor:blue}}>{JSON.stringify(ques)} + {JSON.stringify(answer)}</h1>)}
-      <div>
-        <button onClick={() => jobDone()}>Click me</button>
-        <h1>{sessionStorage.getItem("autosave")}</h1>
-      </div>
-    </div>
-  )
-}
+    if(isBusy){
+      return (
+        <h1>hello</h1>
+      )
+    }
+    console.log(showScore)
+    console.log("question length: ", question.length)
+    console.log("index: ", index)
+
+   return (
+      <>
+          {showScore ? (
+          <Grid container
+          spacing={2}
+          justifyContent="center"
+          alignItems="center">
+            <Grid item>
+              <Typography variant="h1" sx={{color:'#b71c1c'}}>{score}</Typography>
+            </Grid>
+          </Grid>) : (<Grid container
+              spacing={2}
+              justifyContent="center"
+              alignItems="center"
+              >
+                <Grid item>
+                  <Typography variant="h2" sx={{mt:20, pb:2, ml:118, color: 'green'}}>{index + 1}/5</Typography>
+                </Grid>
+                
+              <Grid container
+              spacing={2}
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+              sx={{backgroundColor: '#c7e293', mb:20, mx:50}}>
+                <Grid item sx={{pt:5}}>
+                  <Typography variant ='h4'>{ques}</Typography>
+                </Grid>
+                <Grid item sx={{pb:20}}>
+                  <Typography variant='subtitle1'>{name}</Typography>
+                </Grid>
+                <Grid item>
+                <Grid container 
+                rowSpacing={1}
+                justifyContent="center"
+                alignItems={"center"}
+                columnSpacing={{xs:1, sm:2, md:3}}
+                sx={{mb:10, ml:30}}>
+                  {shuffles?.map((shuffle) => (
+                    <Grid item key={shuffle} xs={6}>
+                      <Button
+                      variant='contained'
+                      sx={{backgroundColor: '#1783EF'}}
+                        onClick={() => jobDone(shuffle)}
+                      >{shuffle}</Button>
+                    </Grid>
+                  ))}
+                </Grid>
+                </Grid>
+              </Grid>
+            </Grid>)} 
+      </> 
+      )
+    }
