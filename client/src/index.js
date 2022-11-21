a7611b88f449462ad1f5fe3b73866572c28dda04
Hassan Shirazi <shiraziahassan@gmail.com>
2022-11-20 23:12:50 -0500

    hash router changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,16 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <App/>
+    <HashRouter>
+      <App/>
+    </HashRouter>
   </StrictMode>
 );
 
