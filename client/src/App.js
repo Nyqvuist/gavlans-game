b2323a156586882d54806dba1a486829cc4977e3
Hassan Shirazi <shiraziahassan@gmail.com>
2022-11-21 21:52:18 -0500

    back to normal

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import Main from '../src/components/Main';
-import { HashRouter as Routes, Route, HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Start from '../src/components/Start';
 import Play from '../src/components/Play';
 import Results from '../src/components/Results';
@@ -18,14 +18,14 @@ const theme = createTheme({
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <HashRouter basename='/gavlans-game' >
+      <Router>
         <Routes>
-          <Route path="/" element={<Main/>}/>
+          <Route path="" element={<Main/>}/>
           <Route path="/start" element={<Start/>}/>
           <Route path='/play' element={<Play/>}/>
           <Route path='/results' element={<Results/>}/>
         </Routes>
-      </HashRouter>
+      </Router>
     </ThemeProvider>
   );
 }
