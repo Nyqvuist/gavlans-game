6ec0baef9119db1fe06515569c7319ac4e392443
Hassan Shirazi <shiraziahassan@gmail.com>
2022-11-21 22:17:18 -0500

    changed ip

--- client/src/components/Play.js
+++ client/src/components/Play.js
@@ -58,7 +58,7 @@ export default function Play() {
     useEffect(() => {
       setIsBusy(true)
         const fetchData = async () => {
-            const result = await axios.get(`http://localhost:3001/${location.state}`)
+            const result = await axios.get(`http://173.68.87.222:3001/${location.state}`)
             const jsonResult = result.data
             setQuestion(jsonResult)
             setIsBusy(false)
