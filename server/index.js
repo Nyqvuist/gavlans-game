40e2e283ffc8457aa10ad7669044a6b0956b09fe
Hassan Shirazi <shiraziahassan@gmail.com>
2022-11-22 21:00:28 -0500

    editing cors

--- server/index.js
+++ server/index.js
@@ -4,12 +4,16 @@ const mongoose = require("mongoose");
 require('dotenv').config()
 const cors = require('cors');
 
-app.use(cors())
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 
+  }
+
 const quesRoute = require("./routes/csgo");
-app.use("/csgo", quesRoute);
+app.use("/csgo", cors(corsOptions), quesRoute);
 
 const leagueRoute = require("./routes/league")
-app.use("/league", leagueRoute);
+app.use("/league", cors(corsOptions), leagueRoute);
 
 mongoose.connect(process.env.MONGO, () => {
     console.log("Database is connected.")
