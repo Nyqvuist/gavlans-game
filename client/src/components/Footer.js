2a621a5648be6c03118e66465064537ccfa75838
Hassan Shirazi <shiraziahassan@gmail.com>
2022-11-28 16:17:19 -0500

    footer changes

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,33 +4,60 @@ import EmailIcon from '@mui/icons-material/Email';
 import { Divider, Grid } from '@mui/material';
 import { Typography } from '@mui/material';
 import logo from "../assets/Hassan_Lion_Logo_Alt.png";
+import "../styles/Divider.css";
 
 
 export default function Footer() {
   return (
     <Grid container
           direction="row"
-          justifyContent="center"
+          justifyContent="space-evenly"
           alignItems="center"
-          sx={{mt: 5}}
           className="footer">
-            <Grid item sx={{mr: 72}}>
-              <img src={logo} alt='logo' width='70' height='70'/> 
+            <Grid item>
+              <Grid container
+              direction={'row'}>
+                <Grid item>
+                  <img src={logo} alt='logo' width='70' height='70'/> 
+                </Grid>
+              </Grid>
             </Grid>
             <Grid item>
-              <Typography variant='subtitle2' sx={{color:'#D9DCD6'}}>Terms of Service</Typography>
+              <Grid container
+              spacing={0.5}
+              direction={'row'}
+              sx={{mx: 5}}>
+                <Grid item>
+                  <Typography variant='subtitle2' sx={{color:'#D9DCD6'}}>Terms of Service</Typography>
+                </Grid>
+                <Grid item>
+                  <Divider orientation='vertical'/>
+                </Grid>
+                <Grid item>
+                  <Typography variant='subtitle2' sx={{color:'#D9DCD6'}}>Privacy</Typography>
+                </Grid>
+                <Grid item>
+                  <Divider orientation='vertical'/>
+                </Grid>
+                <Grid item>
+                  <Typography variant='subtitle2' sx={{color:'#D9DCD6'}}>Contact</Typography>
+                </Grid>
+                <Grid item>
+                  <Divider orientation='vertical'/>
+                </Grid>
+                <Grid item>
+                  <Typography variant='subtitle2' sx={{color:'#D9DCD6'}}>Donation</Typography>
+                </Grid>
+              </Grid>
             </Grid>
-            <Divider orientation='vertical' variant='middle' sx={{color:'#D9DCD6', mx: 0.2}}/>
             <Grid item>
-              <Typography variant='subtitle2' sx={{color:'#D9DCD6'}}>Privacy</Typography>
-            </Grid>
-            <Divider orientation='vertical' variant='middle' sx={{color:'#D9DCD6', mx: 0.2}}/>
-            <Grid item>
-              <Typography variant='subtitle2' sx={{color:'#D9DCD6'}}>Contact</Typography>
-            </Grid>
-            <Grid item sx={{ml: 70}}>
-              <GitHubIcon sx={{color:'#D9DCD6', mx: 2}}/>
-              <EmailIcon sx={{color:'#D9DCD6'}}/>
+              <Grid container
+              direction={'row'}>
+                <Grid item>
+                  <GitHubIcon sx={{color:'#D9DCD6', mx: 2}}/>
+                  <EmailIcon sx={{color:'#D9DCD6'}}/>
+                </Grid>
+              </Grid>
             </Grid>
     </Grid>
   )
