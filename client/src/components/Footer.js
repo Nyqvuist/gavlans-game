d30dcaea76cc0e35cbcc334c54fda32a0f48927b
Hassan Shirazi <shiraziahassan@gmail.com>
2022-11-28 19:52:25 -0500

    links

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
-import { Divider, Grid } from '@mui/material';
+import { Divider, Grid, Link } from '@mui/material';
 import { Typography } from '@mui/material';
 import logo from "../assets/Hassan_Lion_Logo_Alt.png";
 import "../styles/Divider.css";
@@ -40,13 +40,17 @@ export default function Footer() {
                   <Divider orientation='vertical'/>
                 </Grid>
                 <Grid item>
-                  <Typography variant='subtitle2' sx={{color:'#D9DCD6'}}>Contact</Typography>
+                  <Link href='mailto:gavlansgamehelp@gmail.com' sx={{textDecoration: 'none'}}>
+                    <Typography variant='subtitle2' sx={{color:'#D9DCD6'}}>Contact</Typography>
+                  </Link>
                 </Grid>
                 <Grid item>
                   <Divider orientation='vertical'/>
                 </Grid>
                 <Grid item>
-                  <Typography variant='subtitle2' sx={{color:'#D9DCD6'}}>Donation</Typography>
+                  <a className="donation" href='https://www.paypal.com/donate/?hosted_button_id=XWHFWVRD9UVRC'>
+                    <Typography variant='subtitle2' sx={{color:'#D9DCD6'}}>Donation</Typography>
+                  </a>
                 </Grid>
               </Grid>
             </Grid>
@@ -54,8 +58,12 @@ export default function Footer() {
               <Grid container
               direction={'row'}>
                 <Grid item>
-                  <GitHubIcon sx={{color:'#D9DCD6', mx: 2}}/>
-                  <EmailIcon sx={{color:'#D9DCD6'}}/>
+                  <a className="donation" href="https://github.com/Nyqvuist">
+                    <GitHubIcon sx={{color:'#D9DCD6', mx: 2}}/>
+                  </a>
+                  <Link href='mailto:gavlansgamehelp@gmail.com'>
+                    <EmailIcon sx={{color:'#D9DCD6'}}/>
+                  </Link>
                 </Grid>
               </Grid>
             </Grid>
