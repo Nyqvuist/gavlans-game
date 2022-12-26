79d1b63fd7722369a1b2023daf926b5af6b7e152
Hassan Shirazi <shiraziahassan@gmail.com>
2022-12-26 17:44:09 -0500

    adjusted mobile view

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,72 +1,83 @@
-import React from 'react'
-import GitHubIcon from '@mui/icons-material/GitHub';
-import EmailIcon from '@mui/icons-material/Email';
-import { Divider, Grid, Link } from '@mui/material';
-import { Typography } from '@mui/material';
+import React from "react";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
+import { Divider, Grid, Link } from "@mui/material";
+import { Typography } from "@mui/material";
 import logo from "../assets/Hassan_Lion_Logo_Alt.png";
 import "../styles/Divider.css";
-
+import "../styles/footer.css";
 
 export default function Footer() {
   return (
-    <Grid container
-          direction="row"
-          justifyContent="space-evenly"
-          alignItems="center"
-          className="footer">
-            <Grid item>
-              <Grid container
-              direction={'row'}>
-                <Grid item>
-                  <img src={logo} alt='logo' width='70' height='70'/> 
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Grid container
-              spacing={0.5}
-              direction={'row'}
-              sx={{mx: 5}}>
-                <Grid item>
-                  <Typography variant='subtitle2' sx={{color:'#D9DCD6'}}>Terms of Service</Typography>
-                </Grid>
-                <Grid item>
-                  <Divider orientation='vertical'/>
-                </Grid>
-                <Grid item>
-                  <Typography variant='subtitle2' sx={{color:'#D9DCD6'}}>Privacy</Typography>
-                </Grid>
-                <Grid item>
-                  <Divider orientation='vertical'/>
-                </Grid>
-                <Grid item>
-                  <Link href='mailto:gavlansgamehelp@gmail.com' sx={{textDecoration: 'none'}}>
-                    <Typography variant='subtitle2' sx={{color:'#D9DCD6'}}>Contact</Typography>
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Divider orientation='vertical'/>
-                </Grid>
-                <Grid item>
-                  <a className="donation" href='https://www.paypal.com/donate/?hosted_button_id=XWHFWVRD9UVRC'>
-                    <Typography variant='subtitle2' sx={{color:'#D9DCD6'}}>Donation</Typography>
-                  </a>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Grid container
-              direction={'row'}>
-                <Grid item>
-                  <a className="donation" href="https://github.com/Nyqvuist">
-                    <GitHubIcon sx={{color:'#D9DCD6', mx: 2}}/>
-                  </a>
-                  <Link href='mailto:gavlansgamehelp@gmail.com'>
-                    <EmailIcon sx={{color:'#D9DCD6'}}/>
-                  </Link>
-                </Grid>
-              </Grid>
-            </Grid>
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-evenly"
+      alignItems="center"
+      className="footer"
+    >
+      <Grid item>
+        <Grid container direction={"row"}>
+          <Grid item>
+            <img src={logo} alt="logo" width="70" height="70" />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Grid container spacing={0.5} direction={"row"} sx={{ mx: 5 }}>
+          <Grid item>
+            <Typography variant="subtitle2" sx={{ color: "#D9DCD6" }}>
+              Terms of Service
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Divider orientation="vertical" />
+          </Grid>
+          <Grid item>
+            <Typography variant="subtitle2" sx={{ color: "#D9DCD6" }}>
+              Privacy
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Divider orientation="vertical" />
+          </Grid>
+          <Grid item>
+            <Link
+              href="mailto:gavlansgamehelp@gmail.com"
+              sx={{ textDecoration: "none" }}
+            >
+              <Typography variant="subtitle2" sx={{ color: "#D9DCD6" }}>
+                Contact
+              </Typography>
+            </Link>
+          </Grid>
+          <Grid item>
+            <Divider orientation="vertical" />
+          </Grid>
+          <Grid item>
+            <a
+              className="donation"
+              href="https://www.paypal.com/donate/?hosted_button_id=XWHFWVRD9UVRC"
+            >
+              <Typography variant="subtitle2" sx={{ color: "#D9DCD6" }}>
+                Donation
+              </Typography>
+            </a>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Grid container direction={"row"}>
+          <Grid item>
+            <a className="donation" href="https://github.com/Nyqvuist">
+              <GitHubIcon sx={{ color: "#D9DCD6", mx: 2 }} />
+            </a>
+            <Link href="mailto:gavlansgamehelp@gmail.com">
+              <EmailIcon sx={{ color: "#D9DCD6" }} />
+            </Link>
+          </Grid>
+        </Grid>
+      </Grid>
     </Grid>
-  )
+  );
 }
--- client/src/components/Icons.js
+++ client/src/components/Icons.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/icons.css";
 import { Grid, Typography, useMediaQuery, Zoom } from "@mui/material";
 import { NavLink } from "react-router-dom";
@@ -15,27 +15,25 @@ import "../styles/Tooltip.css";
 import { Box } from "@mui/system";
 
 export default function Icons() {
-  const [open, setOpen] = useState(true);
-
   const tooltips = useMediaQuery("(max-width: 479px)");
 
   return (
     <>
       <Grid
         container
-        spacing={12}
+        spacing={tooltips ? 8 : 12}
         className="first-row"
         direction="row"
         justifyContent="center"
         alignItems="center"
-        sx={{ mb: 2 }}
+        sx={{ mb: 8 }}
       >
         <Grid item>
           <>
             {tooltips ? (
               <>
                 <div className="container-box">
-                  <Box className="icons-box">
+                  <Box className="icons-box" sx={{ mb: 1 }}>
                     <Typography className="text-box">CS:GO</Typography>
                   </Box>
                 </div>
@@ -67,78 +65,99 @@ export default function Icons() {
           </>
         </Grid>
         <Grid item>
-          <NavLink
-            to="/start"
-            state={{ game: ["league"], title: ["league of legends"] }}
-          >
-            {tooltips ? (
-              <Tooltip
-                title={<h1 className="tooltip-h1">LEAGUE</h1>}
-                open={open}
-                placement="top"
+          {tooltips ? (
+            <>
+              <div className="container-box">
+                <Box className="icons-box" sx={{ mb: 1 }}>
+                  <Typography className="text-box">LEAGUE</Typography>
+                </Box>
+              </div>
+              <NavLink
+                to="/start"
+                state={{ game: ["league"], title: ["league of legends"] }}
               >
                 <img className="img" src={league} alt="league" />
-              </Tooltip>
-            ) : (
-              <Tooltip
-                title={<h1 className="tooltip-h1">LEAGUE</h1>}
-                TransitionComponent={Zoom}
-                placement="top"
+              </NavLink>
+            </>
+          ) : (
+            <Tooltip
+              title={<h1 className="tooltip-h1">LEAGUE</h1>}
+              TransitionComponent={Zoom}
+              placement="top"
+            >
+              <NavLink
+                to="/start"
+                state={{ game: ["league"], title: ["league of legends"] }}
               >
                 <img className="img" src={league} alt="league" />
-              </Tooltip>
-            )}
-          </NavLink>
+              </NavLink>
+            </Tooltip>
+          )}
         </Grid>
         <Grid item className="container">
-          <NavLink to="/start" state={{ game: ["gow"], title: ["god of war"] }}>
-            {tooltips ? (
-              <Tooltip
-                title={<h1 className="tooltip-h1">GOD OF WAR</h1>}
-                open={open}
-                placement="top"
+          {tooltips ? (
+            <>
+              <div className="container-box">
+                <Box className="icons-box" sx={{ mb: 1 }}>
+                  <Typography className="text-box">GOD OF WAR</Typography>
+                </Box>
+              </div>
+              <NavLink
+                to="/start"
+                state={{ game: ["gow"], title: ["god of war"] }}
               >
                 <img className="img" src={gow} alt="gow" />
-              </Tooltip>
-            ) : (
-              <Tooltip
-                title={<h1 className="tooltip-h1">GOD OF WAR</h1>}
-                TransitionComponent={Zoom}
-                placement="top"
+              </NavLink>
+            </>
+          ) : (
+            <Tooltip
+              title={<h1 className="tooltip-h1">GOD OF WAR</h1>}
+              TransitionComponent={Zoom}
+              placement="top"
+            >
+              <NavLink
+                to="/start"
+                state={{ game: ["gow"], title: ["god of war"] }}
               >
                 <img className="img" src={gow} alt="gow" />
-              </Tooltip>
-            )}
-          </NavLink>
+              </NavLink>
+            </Tooltip>
+          )}
         </Grid>
         <Grid item>
-          <NavLink
-            to="/start"
-            state={{ game: ["souls"], title: ["dark souls"] }}
-          >
-            {tooltips ? (
-              <Tooltip
-                title={<h1 className="tooltip-h1">DARK SOULS</h1>}
-                open={open}
-                placement="top"
+          {tooltips ? (
+            <>
+              <div className="container-box">
+                <Box className="icons-box" sx={{ mb: 1 }}>
+                  <Typography className="text-box">DARK SOULS</Typography>
+                </Box>
+              </div>
+              <NavLink
+                to="/start"
+                state={{ game: ["souls"], title: ["dark souls"] }}
               >
                 <img className="img" src={souls} alt="souls" />
-              </Tooltip>
-            ) : (
-              <Tooltip
-                title={<h1 className="tooltip-h1">DARK SOULS</h1>}
-                TransitionComponent={Zoom}
-                placement="top"
+              </NavLink>
+            </>
+          ) : (
+            <Tooltip
+              title={<h1 className="tooltip-h1">DARK SOULS</h1>}
+              TransitionComponent={Zoom}
+              placement="top"
+            >
+              <NavLink
+                to="/start"
+                state={{ game: ["souls"], title: ["dark souls"] }}
               >
                 <img className="img" src={souls} alt="souls" />
-              </Tooltip>
-            )}
-          </NavLink>
+              </NavLink>
+            </Tooltip>
+          )}
         </Grid>
       </Grid>
       <Grid
         container
-        spacing={12}
+        spacing={tooltips ? 8 : 12}
         className="second-row"
         direction="row"
         justifyContent="center"
@@ -146,94 +165,124 @@ export default function Icons() {
         sx={{ mb: 5 }}
       >
         <Grid item>
-          <NavLink
-            to="/start"
-            state={{ game: ["football"], title: ["football"] }}
-          >
-            {tooltips ? (
-              <Tooltip
-                title={<h1 className="tooltip-h1">FOOTBALL</h1>}
-                open={open}
-                placement="bottom"
+          {tooltips ? (
+            <>
+              <div className="container-box">
+                <Box className="icons-box" sx={{ mb: 1 }}>
+                  <Typography className="text-box">FOOTBALL</Typography>
+                </Box>
+              </div>
+              <NavLink
+                to="/start"
+                state={{ game: ["football"], title: ["football"] }}
               >
                 <img className="img" src={football} alt="football" />
-              </Tooltip>
-            ) : (
-              <Tooltip
-                title={<h1 className="tooltip-h1">FOOTBALL</h1>}
-                TransitionComponent={Zoom}
-                placement="bottom"
+              </NavLink>
+            </>
+          ) : (
+            <Tooltip
+              title={<h1 className="tooltip-h1">FOOTBALL</h1>}
+              TransitionComponent={Zoom}
+              placement="bottom"
+            >
+              <NavLink
+                to="/start"
+                state={{ game: ["football"], title: ["football"] }}
               >
                 <img className="img" src={football} alt="football" />
-              </Tooltip>
-            )}
-          </NavLink>
+              </NavLink>
+            </Tooltip>
+          )}
         </Grid>
         <Grid item>
-          <NavLink to="/start" state={{ game: ["hockey"], title: ["hockey"] }}>
-            {tooltips ? (
-              <Tooltip
-                title={<h1 className="tooltip-h1">HOCKEY</h1>}
-                open={open}
-                placement="bottom"
+          {tooltips ? (
+            <>
+              <div className="container-box">
+                <Box className="icons-box" sx={{ mb: 1 }}>
+                  <Typography className="text-box">HOCKEY</Typography>
+                </Box>
+              </div>
+              <NavLink
+                to="/start"
+                state={{ game: ["hockey"], title: ["hockey"] }}
               >
                 <img className="img" src={hockey} alt="hockey" />
-              </Tooltip>
-            ) : (
-              <Tooltip
-                title={<h1 className="tooltip-h1">HOCKEY</h1>}
-                TransitionComponent={Zoom}
-                placement="bottom"
+              </NavLink>
+            </>
+          ) : (
+            <Tooltip
+              title={<h1 className="tooltip-h1">HOCKEY</h1>}
+              TransitionComponent={Zoom}
+              placement="bottom"
+            >
+              <NavLink
+                to="/start"
+                state={{ game: ["hockey"], title: ["hockey"] }}
               >
                 <img className="img" src={hockey} alt="hockey" />
-              </Tooltip>
-            )}
-          </NavLink>
+              </NavLink>
+            </Tooltip>
+          )}
         </Grid>
         <Grid item className="container">
-          <NavLink to="/start" state={{ game: ["soccer"], title: ["soccer"] }}>
-            {tooltips ? (
-              <Tooltip
-                title={<h1 className="tooltip-h1">SOCCER</h1>}
-                open={open}
-                placement="bottom"
+          {tooltips ? (
+            <>
+              <div className="container-box">
+                <Box className="icons-box" sx={{ mb: 1 }}>
+                  <Typography className="text-box">SOCCER</Typography>
+                </Box>
+              </div>
+              <NavLink
+                to="/start"
+                state={{ game: ["soccer"], title: ["soccer"] }}
               >
                 <img className="img" src={soccer} alt="soccer" />
-              </Tooltip>
-            ) : (
-              <Tooltip
-                title={<h1 className="tooltip-h1">SOCCER</h1>}
-                TransitionComponent={Zoom}
-                placement="bottom"
+              </NavLink>
+            </>
+          ) : (
+            <Tooltip
+              title={<h1 className="tooltip-h1">SOCCER</h1>}
+              TransitionComponent={Zoom}
+              placement="bottom"
+            >
+              <NavLink
+                to="/start"
+                state={{ game: ["soccer"], title: ["soccer"] }}
               >
                 <img className="img" src={soccer} alt="soccer" />
-              </Tooltip>
-            )}
-          </NavLink>
+              </NavLink>
+            </Tooltip>
+          )}
         </Grid>
         <Grid item>
-          <NavLink
-            to="/start"
-            state={{ game: ["basketball"], title: ["basketball"] }}
-          >
-            {tooltips ? (
-              <Tooltip
-                title={<h1 className="tooltip-h1">BASKETBALL</h1>}
-                open={open}
-                placement="bottom"
+          {tooltips ? (
+            <>
+              <div className="container-box">
+                <Box className="icons-box" sx={{ mb: 1 }}>
+                  <Typography className="text-box">BASKETBALL</Typography>
+                </Box>
+              </div>
+              <NavLink
+                to="/start"
+                state={{ game: ["basketball"], title: ["basketball"] }}
               >
                 <img className="img" src={basketball} alt="basketball" />
-              </Tooltip>
-            ) : (
-              <Tooltip
-                title={<h1 className="tooltip-h1">BASKETBALL</h1>}
-                TransitionComponent={Zoom}
-                placement="bottom"
+              </NavLink>
+            </>
+          ) : (
+            <Tooltip
+              title={<h1 className="tooltip-h1">BASKETBALL</h1>}
+              TransitionComponent={Zoom}
+              placement="bottom"
+            >
+              <NavLink
+                to="/start"
+                state={{ game: ["basketball"], title: ["basketball"] }}
               >
                 <img className="img" src={basketball} alt="basketball" />
-              </Tooltip>
-            )}
-          </NavLink>
+              </NavLink>
+            </Tooltip>
+          )}
         </Grid>
       </Grid>
     </>
